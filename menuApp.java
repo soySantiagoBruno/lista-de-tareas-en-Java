29c4c0c5c49525e71dd1df433c0c6f42c3a57456
Santiago <soysantiagobruno@gmail.com>
2022-10-24 16:47:21 -0300

    primer commit

---
+++ menuApp.java
@@ -0,0 +1,19 @@
+package AppListaTareas;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
+
+public class menuApp {
+
+    Operaciones operaciones = new Operaciones();
+    public menuApp() throws IOException {
+
+        Formulario formulario = new Formulario();
+
+        formulario.setBounds(0,0,600,400);
+        formulario.setVisible(true);
+        formulario.setLocationRelativeTo(null);
+
+    }
+}
